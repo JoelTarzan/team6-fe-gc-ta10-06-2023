a7225fd756bfec89bc4e487ae48fedea823bbce0
JesusDavid2002 <jesusdavidsepu@gmail.com>
2023-06-28 16:28:44 +0200

    update

---
+++ Reto3/src/reto3.ts
@@ -0,0 +1,16 @@
+
+class Raices {
+    //1-Atributos de la class
+    private a:number;
+    private b:number;
+    private c:number;
+
+    //2-Constructor
+    constructor(newA:number,newB:number,newC:number){
+        this.a = newA;
+        this.b = newB;
+        this.c = newC;
+    }
+
+    //3-Métodos de la class
+}
